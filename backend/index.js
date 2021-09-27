4d5a09de0319bd6f34ea56da2b761c40d659d659
pranjal-prateek <81037562+pranjal-prateek@users.noreply.github.com>
2021-09-27 10:01:12 +0530

    second commit

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const connectToMongo=require('./db.js');
+connectToMongo();
+const port=3000;
+const app=express();
+app.use(express.json())
+app.get('/',(req,res)=>{
+    res.send(`hello i am from port no, ${port}`)
+})
+//Available Routes
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
+
+
+app.listen(port,()=>{
+    console.log(`listening to port no ${port}`);
+})
